abb304e264a548ee12adde36c1d34755e9e655f2
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-05-02 13:32:16 +0300

    routes on bun

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
 
-const z:number=12
-const read=(text:string)=>{
-    return text
-} 
+const server=Bun.serve({
+    port:3000,
+    fetch(req:Request){
+        const url=new URL(req.url)
+        if (url.pathname=='/') {
+            const body=figlet.textSync('Fazliddin Khayrullaev')
 
-console.log(read("Hello Fazliddin  "+z));
+            return new Response(body)
+        }
+
+        if (url.pathname==='/about') {
+
+            return new Response("About me")
+        }
+
+        if (url.pathname==='/contact') {
+
+            return new Response("Contact me")
+        }
+        
+        
+      
+
+
+        return new Response('404!')
+    }
+})
+
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
